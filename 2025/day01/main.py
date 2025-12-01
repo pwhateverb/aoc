3e7e535da6ceb38d0ba41d4604ab4f6cc7be440f
Parisa Babaei <babaeip00@gmail.com>
2025-12-01 22:01:37 +0100

    Add 2025 day1 part2

--- 2025/day01/main.py
+++ 2025/day01/main.py
@@ -1,28 +1,35 @@
-def rotate(is_positive, current, jump):
-    answer = 0
-    if is_positive:
-        answer = (current + jump) % 100
-    else:
-        answer = current - (jump % 100)
-        if answer < 0:
-            answer += 100
-    return answer
-
-def part_one():
+def part_one(file):
     current = 50
     count = 0
+    for line in file:
+        jump = int(line[1:])
+        if line[0] == "R":
+            current = (current + jump) % 100
+        else:
+            current = (current - jump) % 100
+        if current == 0:
+            count += 1
+    print(count)
 
-    with open(r"2025\day01\input.txt") as f:
-        for line in f:
-            if line[0] == "R":
-                current = rotate(True, current, int(line[1:]))
-            else:
-                current = rotate(False, current, int(line[1:]))
-            if current == 0:
+
+def part_two(file):
+    current = 50
+    count = 0
+    for line in file:
+        jump = int(line[1:])
+        if line[0] == "R":
+            count += (current + jump) // 100
+            current = (current + jump) % 100
+        else:
+            count += (jump // 100)
+            if current <= (jump % 100) and current != 0:
                 count += 1
-
+            current = (current - jump) % 100
     print(count)
 
 
 if __name__ == "__main__":
-    part_one()
+    file = open(r"2025\day01\input.txt")
+    part_one(file)
+    part_two(file)
+    file.close()
